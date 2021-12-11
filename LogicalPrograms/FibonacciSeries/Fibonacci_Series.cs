3fb296502646b6f25865f1fd0f59fcf77ace6e0f
unknown <hindhujhasridhar@gmail.com>
2021-12-11 14:35:51 +0530

    FIBONACCI SERIES

---
+++ LogicalPrograms/FibonacciSeries/Fibonacci_Series.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms.FibonacciSeries
+{
+    internal class Fibonacci_Series
+    {
+        public void Fibonacci()
+        {
+
+            int num1=0, num2=1, temp,number;
+            Console.WriteLine("ENTER NUMBER OF ELEMENTS");
+            number=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine( "" + num1 ,"" + num2);
+            for(int i=2;i<=number;i++)
+            {
+                temp = num1 + num2;
+                Console.WriteLine(temp);
+                num1 = num2;
+                num2= temp;
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LOGICAL PROGRAMS");
+            while (true)
+            {
+                Console.WriteLine("-----------------");
+                Console.WriteLine("1.FIBONACCI SERIES");
+                Console.WriteLine("ENTER YOUR OPTION");
+                int select = Convert.ToInt32(Console.ReadLine());
+
+                switch (select)
+                {
+                    case 1:
+                   FibonacciSeries.Fibonacci_Series fibo=new FibonacciSeries.Fibonacci_Series();
+                        fibo.Fibonacci();
+                        break;
+                    default:
+                        Console.WriteLine("ENTER CORRECT OPTION: ");
+                        break;
+                }
+            }
+        }
+    }
+}
